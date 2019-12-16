69af1de118484e3aeaebd608c1bb4023a98c6b96
nbert100 <nbert14@gmail.com>
2019-12-16 01:19:57 -0500

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 
 // Write your code here
+
+let num1 = 2
+let num2 = 31
+let multiply = num1 * num2
+
+let random = Math.ceil(Math.random());
+
+let mod = 11 % 7
+
+let max = Math.max(1, 2, 3, 20)
